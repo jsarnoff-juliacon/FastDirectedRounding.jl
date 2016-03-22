93050fa5ea84644bb6efa042825ad92eda8f6226
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-22 09:25:21 -0400

    Update eftRound.jl

--- src/eftRound.jl
+++ src/eftRound.jl
@@ -1,1 +1,18 @@
+#=
+     using the result of an errorfree transformation to drive highly performant multi-directed roundings
+=#
 
+@inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) = 
+     (((signbit(hi)==signbit(lo)) | (lo==zero(T))) ? hi : nextNearerToZero(hi))
+
+@inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) = 
+    (((signbit(hi)!=signbit(lo)) | (lo == zero(T))) ? hi : nextAwayFromZero(hi))
+
+@inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) = 
+    (((signbit(lo) | (lo == zero(T)))) ? hi : nextFloat(hi))
+
+@inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) = 
+    (((signbit(lo) & (lo != zero(T)))) ? prevFloat(hi) : hi)
+
+@inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = 
+    (hi)
