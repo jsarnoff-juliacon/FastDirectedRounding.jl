7560a749c90440296808d88c29c9581aa37bf19e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 03:07:37 -0400

    Update DirectedRounding.jl

--- src/DirectedRounding.jl
+++ src/DirectedRounding.jl
@@ -1,10 +1,10 @@
 module DirectedRounding
 
 import Base: (+),(-),(*),(/),sqrt
-import ErrorfreeArithmetic: eftAdd, eftSub, eftMul, eftDivApprox, eftSquare #, eftSqrtApprox
+import ErrorfreeArithmetic: eftAdd, eftSub, eftMul, eftDivApprox, eftSqr, eftSqrtApprox
 using AdjacentFloat
 
-export (+),(-),(*),(/),sqrt,square
+export (+),(-),(*),(/),sqrt,sqr
 
 typealias Float Union{Float64,Float32,Float16}
 
